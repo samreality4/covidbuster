a74aeccda4bd8b52566a9f1815df86fc5a27824e
sam <samreality4@gmail.com>
2020-07-22 14:48:02 -0700

    added shooting laser

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ BG = pygame.transform.scale(pygame.image.load(
 
 class Cell:
     COOLDOWN = 30
+
     def __init__(self, x, y, health=100):
         self.x = x
         self.y = y
@@ -42,6 +43,8 @@ class Cell:
 
     def draw(self, window):
         window.blit(self.cell_img, (self.x, self.y))
+        for laser in self.lasers:
+            laser.draw(window)
 
     def get_width(self):
         return self.cell_img.get_width()
@@ -49,18 +52,27 @@ class Cell:
     def get_height(self):
         return self.cell_img.get_height()
 
+    def move_lasers(self, vel, obj):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -= 10
+                self.lasers.remove(laser)
+
     def cooldown(self):
         if self.cool_down_counter >= self.COOLDOWN:
-            self.cool_down_counter
+            self.cool_down_counter = 0
         elif self.cool_down_counter > 0:
             self.cool_down_counter += 1
 
     def shoot(self):
         if self.cool_down_counter == 0:
-            laser = Laser(x, y, self.laser_img)
+            laser = Laser(self.x, self.y, self.laser_img)
             self.lasers.append(laser)
             self.cool_down_counter = 1
-            #TODO x and y not defined
 
 
 class Player(Cell):
@@ -70,6 +82,18 @@ class Player(Cell):
         self.laser_img = YELLOW_LASER
         self.mask = pygame.mask.from_surface(self.cell_img)
         self.max_health = health
+
+    def move_lasers(self, vel, objs):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            else:
+                for obj in objs:
+                    if laser.collision(obj):
+                        objs.remove(obj)
+                        self.lasers.remove(laser)
 
 
 class Enemy(Cell):
@@ -88,7 +112,7 @@ class Enemy(Cell):
         self.y += vel
 
 
-class Laser(Cell):
+class Laser:
     def __init__(self, x, y, img):
         self.x = x
         self.y = y
@@ -96,21 +120,21 @@ class Laser(Cell):
         self.mask = pygame.mask.from_surface(self.img)
 
     def draw(self, window):
-        window.blit(self.img, (self.x, self.y))
+         window.blit(self.img, (self.x, self.y))
 
     def move(self, vel):
         self.y += vel
 
     def off_screen(self, height):
-        return self.y <= height and self.y >= 0
+        return not(self.y <= height and self.y >= 0)
 
     def collision(self, obj):
         return collide(self, obj)
 
 
 def collide(obj1, obj2):
-    offset_x = obj2.x - obj1.x
-    offset_y = obj2.y - obj1.y
+    offset_x = int(obj2.x - obj1.x)
+    offset_y = int(obj2.y - obj1.y)
     return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
 
@@ -129,6 +153,8 @@ def main():
     enemy_vel = 1
 
     player_vel = 5
+
+    laser_vel = 5
 
     player = Player(WIDTH/2 - CELL.get_width()/2, 500)
 
@@ -190,12 +216,18 @@ def main():
             player.y -= player_vel
         if keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() < HEIGHT:
             player.y += player_vel
+        if keys[pygame.K_SPACE]:
+            player.shoot()
 
         for enemy in enemies[:]:
             enemy.move(enemy_vel)
+            enemy.move_lasers(laser_vel, player)
             if enemy.y + enemy.get_height() > HEIGHT:
                 lives -= 1
                 enemies.remove(enemy)
+
+
+        player.move_lasers(laser_vel, enemies)       
 
         redraw_window()
 
